9b139e3035f7e72f4511f0d4fd8fd5676c48d74d
Yosaton Tungmanelatkul <y.tungmanelatkul@gmail.com>
2018-11-13 12:45:46 +0800

    fix

--- db/migrate/20181105103001_change_price_datatype.rb
+++ db/migrate/20181105103001_change_price_datatype.rb
@@ -1,7 +1,7 @@
 class ChangePriceDatatype < ActiveRecord::Migration[5.2]
 
   def change
-    change_column :listings, :price, 'integer USING CAST(price AS integer)'
+    change_column :listings, :price, 'USING price::integer"'
   end
 end
 
