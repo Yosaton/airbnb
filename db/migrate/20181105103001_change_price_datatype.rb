07f5ab87f8c9ac249358a868fd374bc8b1a169fe
Yosaton Tungmanelatkul <y.tungmanelatkul@gmail.com>
2018-11-13 12:36:35 +0800

    price rollback migrate

--- db/migrate/20181105103001_change_price_datatype.rb
+++ db/migrate/20181105103001_change_price_datatype.rb
@@ -2,7 +2,7 @@ class ChangePriceDatatype < ActiveRecord::Migration[5.2]
 
   def change
   end
-  change_column :listings, :price, 'integer USING CAST(price AS integer)'
+  change_column :listings, :price, "USING price::integer"
 end
 
 
